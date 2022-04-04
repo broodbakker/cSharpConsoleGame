29684165730f8a96278ddda2476a8abd6c470c3f
martijn van de ree <mrtjnvanderee@gmail.com>
2022-04-04 13:35:48 +0200

    test

--- monsters.cs
+++ monsters.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace MonsterGenerator
 {
   class Monster
@@ -16,8 +12,6 @@ namespace MonsterGenerator
       hitPoints = monsterHitPoints;
       icon = monsterIcon;
     }
-
-
   }
 }
 
